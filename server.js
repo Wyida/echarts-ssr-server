750e050f1ecaa680b5d214ecda3478e15b16b487
daoying007 <daoying007@gmail.com>
2019-07-02 21:08:22 +0800

    no message

--- server.js
+++ server.js
@@ -1,35 +1,59 @@
-var http = require("http");
-var echarts = require('node-echarts-canvas');
-var url = require("url");
-var server = http.createServer(function(req,res){
-    var arg = url.parse(req.url, true).query;
-    if(!arg.config){
-        res.end('request parameter "config" error');
-        return
-    }
-    try{
-        config = JSON.parse(arg.config)
-    }catch(e){
-        console.log(e)
-    }
+var http = require('http')
+var echarts = require('node-echarts-canvas')
+var url = require('url')
 
-    if(!config.option){
-        res.end('request parameter "config" error');
-        return
+function processConfig (request, callback) {
+  var queryData = ''
+  if (typeof callback !== 'function') {
+    return null
+  }
+  if (request.method === 'GET') {
+    var arg = url.parse(request.url, true).query
+    if (!arg.config) {
+      request.end('request parameter "config" error')
+      return
     }
-
-    var buffer = echarts({
-        option: config.option,
-        width:  config.width || 600,
-        height: config.height || 400,
+    request.config = arg.config
+    callback()
+  } else {
+    request.on('data', function (data) {
+      queryData += data
+      if (queryData.length > 1e6) {
+        request.end('request parameter "config" error')
+      }
     })
-    res.setHeader('Content-Type','image/png');
-    res.write(buffer)
-    res.end();
-});
+    request.on('end', function () {
+      request.config = queryData
+      callback()
+    })
+  }
+}
 
-var hostName = '0.0.0.0';
-var port = 80;
-server.listen(port, hostName, function(){
-    console.log(`server started at port ${port}`);
-});
+var server = http.createServer(function (request, response) {
+  processConfig(request, function () {
+    var config
+    try {
+      config = JSON.parse(request.config)
+    } catch (e) {
+      console.log(e)
+    }
+    if (!config.option) {
+      response.end('request parameter "config" error')
+      return
+    }
+    var buffer = echarts({
+      option: config.option,
+      width: config.width || 600,
+      height: config.height || 400
+    })
+    response.setHeader('Content-Type', 'image/png')
+    response.write(buffer)
+    response.end()
+  })
+})
+
+var hostName = '0.0.0.0'
+var port = 80
+server.listen(port, hostName, function () {
+  console.log(`server started at port ${port}`)
+})
